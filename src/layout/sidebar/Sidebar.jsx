da01ce76853f1a1966bde4acf487acfcd099ec9c
dressgecko <dressgecko@gmail.com>
2024-05-14 04:31:36 +0500

    refactored sidebar

--- src/layout/sidebar/Sidebar.jsx
+++ src/layout/sidebar/Sidebar.jsx
@@ -6,15 +6,15 @@ import { ViewfinderCircleIcon } from '@heroicons/react/24/outline';
 
 const Sidebar = ({ isOpen }) => {
     const menus = [
-        // { name: "Контент", link: "/mediacontent", icon: VideoCameraIcon },
-        // { name: "Экраны", link: "/screen", icon: ComputerDesktopIcon },
-        // { name: "Камеры", link: "/camera", icon: ViewfinderCircleIcon },
+        { name: "Фильм1", link: "/mediacontent"},
+        { name: "Фильм2", link: "/screen"},
+        { name: "Фильм3", link: "/camera"},
     ];
     return (
         <aside className="sticky top-20 h-screen z-10 flex">
             <div
-                className={`bg-white ${isOpen ? "w-60" : "w-20"
-                    } duration-500 text-black px-4 shadow`}
+                className={`bg-white ${isOpen ? "w-60 px-4" : "w-0"
+                    } duration-500 text-black shadow`}
             >
                 <div className="mt-4 flex flex-col gap-4 relative">
                     {/* Отображение элементов меню. Он используется метод map для перебора массива menus */}
@@ -22,8 +22,7 @@ const Sidebar = ({ isOpen }) => {
                         <Link
                             to={menu?.link}
                             key={i}
-                            className={` ${menu?.margin && "mt-5"
-                                } group flex items-center text-md gap-7 font-medium p-2 hover:bg-gray-200 rounded-md`}
+                            className={"group flex items-center text-md gap-7 font-medium p-2 hover:bg-gray-200 rounded-md"}
                         >
                             {/* Надпись в сайдбаре */}
                             <h2
