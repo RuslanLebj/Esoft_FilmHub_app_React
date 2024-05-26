2de8e877d6a893f52c542bbb128af3900c9f9ca1
dressgecko <dressgecko@gmail.com>
2024-05-26 05:06:45 +0500

    added title components

---
+++ src/components/titles/middleTitle/MiddleTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MiddleTitle = ({ title }) => {
+  return (
+    <h3 className="text-xl text-gray-800 font-semibold">{title}</h3>
+  );
+};
+
+export default MiddleTitle;
---
+++ src/components/titles/smallTitle/SmallTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SmallTitle = ({ title }) => {
+  return (
+    <h2 className="text-sm  text-gray-500 font-semibold">{title}</h2>
+  );
+};
+
+export default SmallTitle;
