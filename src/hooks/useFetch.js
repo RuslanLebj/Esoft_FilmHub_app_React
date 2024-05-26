1c738ba963137c1cdbc8fa628ccdfcdfc3825376
dressgecko <dressgecko@gmail.com>
2024-05-26 05:07:11 +0500

    added custom useFetch component

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+// hooks/useFetchData.js
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(url);
+                setData(response.data);
+            } catch (err) {
+                console.error('Error fetching data:', err);
+                setError('Failed to load data');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData().then(r => "");
+    }, [url]);
+
+    return { data, loading, error };
+};
+
+export default useFetch;
