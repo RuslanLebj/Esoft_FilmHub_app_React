5804834cc577ea17c4719e3605d39c234d6f49a9
dressgecko <dressgecko@gmail.com>
2024-06-06 18:36:47 +0500

    added movie list page and component

--- src/pages/movieListPage/MovieListPage.jsx
+++ src/pages/movieListPage/MovieListPage.jsx
@@ -1,15 +1,18 @@
 import useFetch from "../../hooks/useFetch.js";
+import MovieList from "../../components/lists/movieList/MovieList.jsx";
+import MovieDetailPage from "../movieDetailPage/MovieDetailPage.jsx";
 
 
-const MovieListPage = () => {
+const MovieListPage = (filterValue) => {
     const {data: movieList, loading, error} = useFetch(`/movie?limit=100`);
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>{error}</div>;
 
     return (
-        <>
-        </>
+        <div className="bg-gray-100 w-full min-h-screen gap-4 flex-wrap flex justify-center items-center p-4">
+            <MovieList movieList={movieList} />
+        </div>
     );
 };
 
