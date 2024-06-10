ff0f9bb31adcad691375cdb7283334d06e28ec63
dressgecko <dressgecko@gmail.com>
2024-06-10 06:54:04 +0500

    added loading and error pages to movie list page

--- src/pages/movieListPage/MovieListPage.jsx
+++ src/pages/movieListPage/MovieListPage.jsx
@@ -1,17 +1,18 @@
 import useFetch from "../../hooks/useFetch.js";
 import MovieList from "../../components/lists/movieList/MovieList.jsx";
 import MovieDetailPage from "../movieDetailPage/MovieDetailPage.jsx";
-
+import LoadingPage from "../loadingPage/LoadingPage.jsx";
+import ErrorPage from "../errorPage/ErrorPage.jsx";
 
 const MovieListPage = (filterValue) => {
     const {data: movieList, loading, error} = useFetch(`/movie?limit=100`);
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>{error}</div>;
+    if (loading) return <LoadingPage/>;
+    if (error) return <ErrorPage/>;
 
     return (
         <div className="w-full min-h-screen gap-4 flex-wrap flex justify-center items-center p-4">
-            <MovieList movieList={movieList} />
+            <MovieList movieList={movieList}/>
         </div>
     );
 };
