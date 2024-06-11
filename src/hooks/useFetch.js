70273dd73b8dfe9a56958be9413dc05be8f1f883
dressgecko <dressgecko@gmail.com>
2024-06-11 06:41:56 +0500

    updated useFetch hook with redux dispatch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,37 +1,35 @@
-import {useState, useEffect} from 'react';
+import {useEffect} from 'react';
 import axios from 'axios';
 import config from '../config.js'
+import {useDispatch} from "react-redux";
+import {fetchDataStart, fetchDataSuccess, fetchDataFailure} from '../store/slices/moviesSlice.js'
+
 
 const useFetch = (endpoint) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const dispatch = useDispatch();
 
     useEffect(() => {
-            const fetchData = async () => {
-                try {
-                    console.log('API Key:',  import.meta.env.VITE_API_KEY);
-                    const response = await axios.get(`${config.apiUrl}${endpoint}`, {
-                        headers: {
-                            accept: 'application/json',
-                            'X-API-KEY': import.meta.env.VITE_API_KEY
-                        }
-                    });
-                    setData(response.data.docs);
-                    console.log('Response:',  response.data);
-                } catch (err) {
-                    console.error('Error fetching data:', err);
-                    setError('Failed to load data');
-                } finally {
-                    setLoading(false);
-                }
-            };
+        const fetchData = async () => {
+            try {
+                console.log('API Key:',  import.meta.env.VITE_API_KEY);
+                dispatch(fetchDataStart());
+                const response = await axios.get(`${config.apiUrl}${endpoint}`, {
+                    headers: {
+                        accept: 'application/json',
+                        'X-API-KEY': import.meta.env.VITE_API_KEY
+                    }
+                });
+                console.log('Response:',  response.data);
+                dispatch(fetchDataSuccess(response.data.docs));
+            } catch (err) {
+                console.error('Error fetching data:', err);
+                dispatch(fetchDataFailure(err.message || 'Error fetching data'));
+            }
+        };
 
-            fetchData();
-        },
-        [endpoint]);
-
-    return {data, loading, error};
+        fetchData();
+    }, [dispatch, endpoint]);
+    // [dispatch, endpoint] гарантирует, что хук выполнится при изменении dispatch или endpoint. Это важно для обновления данных при изменении endpoint.
 };
 
 export default useFetch;
