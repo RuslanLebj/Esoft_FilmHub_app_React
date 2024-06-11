b6ce4ccf1d81901140fabfa36ec4a740409f5e53
dressgecko <dressgecko@gmail.com>
2024-06-11 06:43:34 +0500

    added redux selector

--- src/pages/movieListPage/MovieListPage.jsx
+++ src/pages/movieListPage/MovieListPage.jsx
@@ -3,16 +3,19 @@ import MovieList from "../../components/lists/movieList/MovieList.jsx";
 import MovieDetailPage from "../movieDetailPage/MovieDetailPage.jsx";
 import LoadingPage from "../loadingPage/LoadingPage.jsx";
 import ErrorPage from "../errorPage/ErrorPage.jsx";
+import { useSelector } from "react-redux";
 
 const MovieListPage = (filterValue) => {
-    const {data: movieList, loading, error} = useFetch(`/movie?limit=100`);
+    useFetch(`/movie?limit=100`);
+    const { error, loading, data: movies } = useSelector((state) => state.movies);
+    console.log('Movies data from store:',  movies);
 
     if (loading) return <LoadingPage/>;
-    if (error) return <ErrorPage/>;
+    if (loading) return <ErrorPage errorMessage={error} />;
 
     return (
         <div className="w-full min-h-screen gap-4 flex-wrap flex justify-center items-center p-4">
-            <MovieList movieList={movieList}/>
+            <MovieList movies={movies}/>
         </div>
     );
 };
