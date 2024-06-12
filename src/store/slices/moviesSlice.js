60b3eb7581cccb843592d0d267161c91423a78a7
dressgecko <dressgecko@gmail.com>
2024-06-13 04:45:01 +0500

    added async thunc func for data fetching, deleted custom fetch hook

--- src/store/slices/moviesSlice.js
+++ src/store/slices/moviesSlice.js
@@ -1,4 +1,23 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import axios from 'axios';
+import config from '../../config.js'
+
+
+const fetchMovies = createAsyncThunk(
+    'movies/fetch',
+    async () => {
+        // console.log('API Key:',  import.meta.env.VITE_API_KEY);
+        const response = await axios.get(`${config.apiUrl}/movie?limit=100`, {
+            headers: {
+                accept: 'application/json',
+                'X-API-KEY': import.meta.env.VITE_API_KEY
+            }
+        });
+        // console.log('Response:',  response.data);
+        return await response.data.docs;
+    }
+);
+
 
 const initialState = {
     loading: false,
@@ -9,27 +28,29 @@ const initialState = {
 const moviesSlice = createSlice({
         name: 'movies',
         initialState,
-        reducers: {
-            fetchDataStart(state) {
-                state.loading = true;
-                state.error = null;
-            },
-            fetchDataSuccess(state, action) {
-                state.loading = false;
-                state.data = action.payload;
-            },
-            fetchDataFailure(state, action) {
-                state.loading = false;
-                state.error = action.payload;
-            },
+        reducers: {},
+        extraReducers: (builder) => {
+            builder
+                .addCase(fetchMovies.pending, (state) => {
+                    state.loading = true;
+                    state.error = null;
+                })
+                .addCase(fetchMovies.fulfilled, (state, action) => {
+                    state.loading = false;
+                    state.data = action.payload;
+                    // console.log('Movies data from store:',  state.data);
+                })
+                .addCase(fetchMovies.rejected, (state, action) => {
+                    state.loading = false;
+                    state.error = action.error.message;
+                    // console.error('Error fetching data:', state.error);
+                });
         }
     }
 );
 
-export const {
-    fetchDataStart,
-    fetchDataSuccess,
-    fetchDataFailure,
-} = moviesSlice.actions;
+export {fetchMovies};
+
+export const {} = moviesSlice.actions;
 
 export default moviesSlice.reducer;
