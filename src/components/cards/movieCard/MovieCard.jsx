e4f7b77345fbc4498d90a062d8d7c5882dc73ff5
dressgecko <dressgecko@gmail.com>
2024-06-13 05:11:52 +0500

    added composite key to genre list

--- src/components/cards/movieCard/MovieCard.jsx
+++ src/components/cards/movieCard/MovieCard.jsx
@@ -34,8 +34,10 @@ const MovieCard = ({movie}) => {
             {/* Ganres TagButtons */}
             <div className='flex flex-wrap justify-center h-24'>
                 {genres.length > 0 ? (
-                    genres.map(genre => (
-                        <TagButton key={genre.id}>
+                    genres.map((genre, index) => (
+                        // Использование составного ключа т.к. API не предоставляет id к жанрам
+                        // (Потенциальные проблемы с производительностью: Если данные изменяются часто, использование индексов может привести к неэффективному обновлению DOM.)
+                        <TagButton key={`${genre.name}-${index}`}>
                             {genre.name}
                         </TagButton>
                     ))
