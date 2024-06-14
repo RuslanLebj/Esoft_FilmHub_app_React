7fd1a1ef58f8d93ad2c5a65fac5778737b0629f8
dressgecko <dressgecko@gmail.com>
2024-06-15 04:50:13 +0500

    added buttons, icons and handlers for add and remove favorite and watch later movies

--- src/components/cards/movieCard/MovieCard.jsx
+++ src/components/cards/movieCard/MovieCard.jsx
@@ -1,22 +1,59 @@
 import defaultPoster from '../../../assets/default-poster.svg'
 import GenreList from "../../lists/genreList/GenreList.jsx";
+import IconButton from "../../buttons/IconButton.jsx";
+import {useDispatch, useSelector} from 'react-redux';
+import {
+    addToFavorites,
+    removeFromFavorites,
+    addToWatchLater,
+    removeFromWatchLater
+} from "../../../store/slices/moviesSlice.js"
+import {HeartIcon as OutlineHeartIcon, BookmarkIcon as OutlineBookmarkIcon} from '@heroicons/react/24/outline'
+import {HeartIcon as SolidHeartIcon, BookmarkIcon as SolidBookmarkIcon} from '@heroicons/react/24/solid'
 
 const MAX_GENRES_DISPLAY = 4; // Константа для максимального количества выводимых кнопок с жанрами
 
 
 const MovieCard = ({movie}) => {
+
+    const dispatch = useDispatch();
+    const favorites = useSelector((state) => state.movies.favorites);
+    const watchLater = useSelector((state) => state.movies.watch_later);
+
+    // Проверяем, находится ли фильм в списке избранного
+    const isFavorite = favorites.includes(movie.id);
+    // Проверяем, находится ли фильм в списке "посмотреть позже"
+    const isWatchLater = watchLater.includes(movie.id);
+
+    // Обработка клика по кнопке избранного
+    const handleFavoriteClick = () => {
+        if (isFavorite) {
+            dispatch(removeFromFavorites(movie.id));
+        } else {
+            dispatch(addToFavorites(movie.id));
+        }
+    };
+
+    const handleWatchLaterClick = () => {
+        if (isWatchLater) {
+            dispatch(removeFromWatchLater(movie.id));
+        } else {
+            dispatch(addToWatchLater(movie.id));
+        }
+    };
+
     const name = movie?.name || movie.alternativeName; // Проверка наличия названия или использоание альтернативного названия
-    const posterUrl = movie.poster?.previewUrl ||  defaultPoster; // Проверка наличия постера и использование изображения по умолчанию, если URL отсутствует.
+    const posterUrl = movie.poster?.previewUrl || defaultPoster; // Проверка наличия постера и использование изображения по умолчанию, если URL отсутствует.
     const description = movie?.description || 'Описание отсутствует'; // Устанавливаем описание по умолчанию, если оно отсутствует
     const rating = movie.rating?.imdb && movie.rating.imdb !== "0" ? `IMDb: ${movie.rating.imdb}` : 'Оценки отсутствуют'; // Проверка наличия и значения рейтинга
     const genres = movie.genres?.length > 0 ? movie.genres.slice(0, MAX_GENRES_DISPLAY) : []; // Проверка на наличие жанров и взятие слайса обозначенной размерности
 
 
-
     return (
-        <div className="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+        <div
+            className="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
             {/* Preview */}
-            <img className="rounded-xl h-96" src={posterUrl} alt={name} />
+            <img className="rounded-xl h-96" src={posterUrl} alt={name}/>
             <div className="p-2">
                 {/* Name */}
                 <h2 className="font-bold text-lg mb-2 line-clamp-1">
@@ -32,7 +69,18 @@ const MovieCard = ({movie}) => {
                 </p>
             </div>
             {/* Ganres TagButtons */}
-            <GenreList genres={genres} />
+            <GenreList genres={genres}/>
+            {/* Favorite/watch later Buttons */}
+            <div className='flex flex-wrap justify-center'>
+                <IconButton onClick={handleFavoriteClick}>
+                    {isFavorite ? <SolidHeartIcon className="w-6 h-6 text-red-500"/> :
+                        <OutlineHeartIcon className="w-6 h-6 text-gray-500"/>}
+                </IconButton>
+                <IconButton onClick={handleWatchLaterClick}>
+                    {isWatchLater ? <SolidBookmarkIcon className="w-6 h-6 text-blue-500"/> :
+                        <OutlineBookmarkIcon className="w-6 h-6 text-gray-500"/>}
+                </IconButton>
+            </div>
         </div>
     );
 };
