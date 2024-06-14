b8ebfffc65785a13f8df47cc7c8c6fb2ce25393f
dressgecko <dressgecko@gmail.com>
2024-06-15 04:50:53 +0500

    added title

--- src/pages/movieListPage/MovieListPage.jsx
+++ src/pages/movieListPage/MovieListPage.jsx
@@ -3,20 +3,26 @@ import MovieList from "../../components/lists/movieList/MovieList.jsx";
 import LoadingPage from "../loadingPage/LoadingPage.jsx";
 import ErrorPage from "../errorPage/ErrorPage.jsx";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchMovies} from "../../store/slices/moviesSlice.js"
+import {fetchPopularMovies} from "../../store/slices/moviesSlice.js"
+import PageTitle from "../../components/titles/pageTitle/PageTitle.jsx";
 
 const MovieListPage = (filterValue) => {
     const dispatch = useDispatch();
     const {data: movies, loading, error,} = useSelector((state) => state.movies);
     useEffect(() => {
-        dispatch(fetchMovies());
+        dispatch(fetchPopularMovies());
     }, []);
 
     if (loading) return <LoadingPage/>;
     if (error) return <ErrorPage errorMessage={error}/>;
 
     return (
-        <MovieList movies={movies}/>
+        <>
+            <div className="my-3 mx-10">
+                <PageTitle> 250 лучших фильмов </PageTitle>
+            </div>
+            <MovieList movies={movies}/>
+        </>
     );
 };
 
