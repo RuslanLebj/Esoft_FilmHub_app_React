a6cfbe359d67758449f60104f595ce71a3069d45
dressgecko <dressgecko@gmail.com>
2024-06-16 04:06:27 +0500

    added filters usage

--- src/pages/movieListPage/MovieListPage.jsx
+++ src/pages/movieListPage/MovieListPage.jsx
@@ -3,15 +3,16 @@ import MovieList from "../../components/lists/movieList/MovieList.jsx";
 import LoadingPage from "../loadingPage/LoadingPage.jsx";
 import ErrorPage from "../errorPage/ErrorPage.jsx";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchPopularMovies} from "../../store/slices/moviesSlice.js"
+import {fetchMovies} from "../../store/slices/moviesSlice.js"
 import PageTitle from "../../components/titles/pageTitle/PageTitle.jsx";
 
-const MovieListPage = (filterValue) => {
+const MovieListPage = () => {
     const dispatch = useDispatch();
-    const {data: movies, loading, error,} = useSelector((state) => state.movies);
+    const {data: movies, loading, error, filters} = useSelector((state) => state.movies);
+
     useEffect(() => {
-        dispatch(fetchPopularMovies());
-    }, []);
+        dispatch(fetchMovies(filters));
+    }, [filters]); // Добавляем filters в зависимости, чтобы обновлять список при каждом изменении фильтров
 
     if (loading) return <LoadingPage/>;
     if (error) return <ErrorPage errorMessage={error}/>;
