e87d3d3c169a8f0a66c55eeb6c1169dd700b315a
dressgecko <dressgecko@gmail.com>
2024-06-18 07:41:49 +0500

    added index element and redirection to /movies

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Layout from "../layout/Layout.jsx";
 import MovieListPage from "../pages/movieListPage/MovieListPage.jsx";
 import MovieDetailPage from "../pages/movieDetailPage/MovieDetailPage.jsx";
@@ -9,8 +9,9 @@ const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Navigate to="/movies" />} />
                 <Route path="/movies" element={<Layout/>}>
-                    <Route path="/movies" element={<MovieListPage/>}/>
+                    <Route index path="/movies" element={<MovieListPage/>}/>
                     <Route path="/movies/:id" element={<MovieDetailPage />} />
                 </Route>
             </Routes>
