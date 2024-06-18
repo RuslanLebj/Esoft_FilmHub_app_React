48cf00f8ea9e0fab3e814c657cacbf0670bf899d
dressgecko <dressgecko@gmail.com>
2024-06-18 05:19:39 +0500

    added movies filtration by tag button genre

--- src/components/lists/genreList/GenreList.jsx
+++ src/components/lists/genreList/GenreList.jsx
@@ -1,6 +1,16 @@
 import TagButton from "../../buttons/TagButton.jsx";
+import {setFilters, setMovieListName} from "../../../store/slices/moviesSlice.js";
+import {useDispatch} from "react-redux";
 
 const GenreList = ({genres}) => {
+    const dispatch = useDispatch();
+
+    const handleSearchByGenre = (genre) => {
+        console.log("Genre:", genre)
+        const filters = {limit: '25', 'genres.name': genre};
+        dispatch(setFilters(filters));
+        dispatch(setMovieListName(`Жанр: ${genre}`));
+    };
 
     return (
         <div className='flex flex-wrap justify-center h-24'>
@@ -8,7 +18,7 @@ const GenreList = ({genres}) => {
                 genres.map((genre, index) => (
                     // Использование составного ключа т.к. API не предоставляет id к жанрам
                     // (Потенциальные проблемы с производительностью: Если данные изменяются часто, использование индексов может привести к неэффективному обновлению DOM.)
-                    <TagButton key={`${genre.name}-${index}`}>
+                    <TagButton key={`${genre.name}-${index}`} onClick={() => handleSearchByGenre(genre.name)}>
                         {genre.name}
                     </TagButton>
                 ))
